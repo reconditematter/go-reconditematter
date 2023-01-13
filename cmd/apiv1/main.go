4b126cedfaccacee3ff1281e501e5a97f577d7e2
Leonid Kneller <recondite_matter@fastmail.com>
2023-01-13 10:14:42 -0500

    Use BUF Connect to implement the server.

--- cmd/apiv1/main.go
+++ cmd/apiv1/main.go
@@ -2,49 +2,41 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net"
+	"net/http"
 
-	"google.golang.org/grpc"
+	"github.com/bufbuild/connect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"github.com/reconditematter/go-reconditematter/gen/proto/go/reconditematter/v1"
+	"github.com/reconditematter/go-reconditematter/gen/proto/go/reconditematter/v1/reconditematterv1connect"
 	"github.com/reconditematter/go-reconditematter/pkg/randomnames"
 )
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func run() error {
 	listen := "127.0.0.1:8080"
-	listener, err := net.Listen("tcp", listen)
-	if err != nil {
-		return fmt.Errorf("failed to listen on %q: %w", listen, err)
+	server := &ReconditeMatterServer{}
+	mux := http.NewServeMux()
+	path, handler := reconditematterv1connect.NewReconditeMatterServiceHandler(server)
+	mux.Handle(path, handler)
+	log.Println("ReconditeMatterServer started", listen, path)
+	if err := http.ListenAndServe(listen, h2c.NewHandler(mux, &http2.Server{})); err != nil {
+		log.Fatalf("ReconditeMatterServer error %w\n", err)
 	}
-	log.Println("listening on:", listen)
-
-	server := grpc.NewServer()
-	reconditematterv1.RegisterReconditeMatterServiceServer(server, &reconditeMatterServiceServer{})
-	if err := server.Serve(listener); err != nil {
-		return fmt.Errorf("failed to serve gRPC server: %w", err)
-	}
-
-	return nil
 }
 
-type reconditeMatterServiceServer struct {
-	reconditematterv1.UnimplementedReconditeMatterServiceServer
+type ReconditeMatterServer struct {
+	reconditematterv1connect.UnimplementedReconditeMatterServiceHandler
 }
 
-func (s *reconditeMatterServiceServer) RandomNames(
+func (s *ReconditeMatterServer) RandomNames(
 	ctx context.Context,
-	req *reconditematterv1.RandomNamesRequest) (*reconditematterv1.RandomNamesResponse, error) {
-	count := req.GetCount()
+	req *connect.Request[reconditematterv1.RandomNamesRequest],
+) (*connect.Response[reconditematterv1.RandomNamesResponse], error) {
+	count := req.Msg.GetCount()
 	result, err := randomnames.Generate(uint(count))
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, "`count` cannot be greater than 1000")
@@ -60,5 +52,5 @@ func (s *reconditeMatterServiceServer) RandomNames(
 			Gender: result[k].Gender,
 		}
 	}
-	return resp, nil
+	return connect.NewResponse(resp), nil
 }
